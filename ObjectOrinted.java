d7a7ce6dc299cf11d6fc61189ab25e42536e9281
MayankRai89 <raimayank245@gmail.com>
2025-01-08 09:23:41 +0530

    commit

--- ObjectOrinted.java
+++ ObjectOrinted.java
@@ -1,11 +1,27 @@
 public class ObjectOrinted {
  public static void main(String[] args) {
-     pen p = new pen();
-     p.setcolor("Blue");
-     System.out.println(p.color);
-     p.setTip(10);
-     System.out.println(p.tip);
+    //  pen p = new pen();
+    //  p.setcolor("Blue");
+    //  System.out.println(p.color);
+    //  p.setTip(10);
+    //  System.out.println(p.tip);
+
+    BankAcc myAcc = new BankAcc();
+    myAcc.username = "Mayank Rai";
+    // myAcc.password = "Varunk@12"; cant access because password is private class
+    myAcc.setPassword("Varunk@12");
+
  }   
+}
+
+class BankAcc{
+    public String username;
+    private String password;
+    public void setPassword(String pass)
+    {
+        password = pass;
+    }
+
 }
 class pen{
     String color;
